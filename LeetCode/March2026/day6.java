616a5b78cf9d6c3caa3e43e810fa20415e66d7a5
rajesh-ede <rajeshede208@gmail.com>
2026-03-06 11:30:07 +0530

    Day 6 added

---
+++ LeetCode/March2026/day6.java
@@ -0,0 +1,22 @@
+package LeetCode.March2026;
+
+public class day6 {
+    private static boolean AtMostOneSigment(String s){
+        int count =  0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '1' && (i ==0 || s.charAt(i-1) == '0')){
+                count++;
+            }
+        }
+        if(count > 1){
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "00000";
+        boolean ans = AtMostOneSigment(s);
+        System.out.println(ans);
+    }
+    
+}
